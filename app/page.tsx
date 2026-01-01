b4e6510cadf60ee2b378686f622cca0017c902c5
mutlu6114 <mutlumertalim14@gmail.com>
2026-01-02 01:13:42 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import Image from "next/image";
+import { useRef } from "react";
+
+export default function Home() {
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+
+  const handleClick = () => {
+    if (!audioRef.current) return;
+    audioRef.current.currentTime = 0;
+    audioRef.current.play();
+  };
+
+  return (
+    <div className="relative flex min-h-screen items-center justify-center bg-zinc-50 p-2 dark:bg-black">
+
+      <Image
+        className="h-full"
+        src="/mutlu.png"
+        alt="Mutlu görseli"
+        width={1206}
+        height={3245}
+        priority
+      />
+
+      <button
+        onClick={handleClick}
+        className="
+          fixed
+          bottom-6
+          font-extrabold
+          text-2xl
+          left-1/2
+          z-100
+          -translate-x-1/2
+          rounded-full
+          bg-red-500
+          px-3
+          border
+          border-red-700
+          py-3
+          text-white
+          backdrop-blur
+          transition
+          hover:bg-black
+        "
+      >
+       TIKLA BANA
+      </button>
+
+      <audio ref={audioRef} src="/KANDIRDIM.mp3" preload="auto" />
+    </div>
+  );
+}
